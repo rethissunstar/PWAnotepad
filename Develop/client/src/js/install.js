6301da6e09d080f36abb64c8871a43ea08b24d75
rethissunstar <rethissunstar@gmail.com>
2023-08-03 22:54:46 -0400

    got it working after some adjustments to the database

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,23 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
+const textHeader = document.querySelector('h1');
 
-// Logic for installing the PWA
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    installBtn.style.visibility = 'visible';
-    textHeader.textContent = 'Click the button to install!';
+  
+  event.preventDefault();
+
+  butInstall.style.visibility = 'visible';
+  textHeader.textContent = 'Click the button to install!';
+
   // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    event.prompt();
-    installBtn.setAttribute('disabled', true);
-    installBtn.textContent = 'Installed!';
+  butInstall.addEventListener("click", async () => {
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+      return;
+    }
+  
+    promptEvent.prompt();
+  
+    window.deferredPrompt = null;
+  
+    butInstall.classList.toggle("hidden", true);
+  });
+  
 });
-});
 
-
-
-// TODO: Add an handler for the `appinstalled` event
+// TODO: Add a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    textHeader.textContent = 'Successfully installed!';
-    console.log('👍', 'appinstalled', event);
+  textHeader.textContent = 'Successfully installed!';
+  console.log('👍', 'appinstalled', event);
 });
