f88f9ab6d5b6d19d5df871d188c034f01bd5651e
rethissunstar <rethissunstar@gmail.com>
2023-08-06 11:39:06 -0400

    this is the database updating id:1 so that retrieval will work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,6 @@
 import { openDB } from 'idb';
+import { header } from './header';
+
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -27,8 +29,9 @@ export const putDb = async (content, key) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const data = { value: content };
-  const request = store.put(data, key);
+  const data = { id:1, value: content };
+  
+  const request = store.put(data);
   const result = await request;
   console.log('Data saved to the database');
 };
@@ -36,19 +39,23 @@ export const putDb = async (content, key) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (key) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
   const request = store.get(1);
   const result = await request;
+  
+  console.log(result);
+  console.log(result.value);
+  //return result.value.value;
 
-  if (result && result.value && typeof result.value === 'string' && result.value.trim() !== '') {
+  if (result.value && typeof result.value === 'string' && result.value.trim() !== '') {
     console.log("Data retrieved from the database");
-    return result.value;
+    return result.value + header;
   } else {
     console.log("Data not found in the database or is invalid");
-    return null;
+    return "sample" + header;
   }
 };
 
